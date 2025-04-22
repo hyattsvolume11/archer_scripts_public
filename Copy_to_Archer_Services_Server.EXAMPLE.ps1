15c00538d605d45f66b0e1a9c8e3f9b6e1b9cbf2
pmv personal <pv@gouge.us>
2025-04-21 20:09:53 -0400

    updates - 2nd commit

--- Copy_to_Archer_Services_Server.EXAMPLE.ps1
+++ Copy_to_Archer_Services_Server.EXAMPLE.ps1
@@ -1,5 +1,6 @@
+# copy-item powershela command  -- copy csv to local Windows server location 
 #
-# copy-item using powershell -- copy csv to local Windows server location 
+# Copy-Item <local path> \\<remote path>
 
 Copy-Item c://my_archer_scripts_location/the_generated_daily_archer_copy.csv \\TESTING_ARCHER_STAGING_SERVER\Archer_Data_feeds\Nessus_Data\
 Copy-Item c://my_archer_scripts_location/the_generated_daily_archer_copy.csv \\TESTING_ARCHER_loc\Archer_Data_feeds\Nessus_Data\
--- Get_IP_Addr_from_AWS-ALL.v2.py
+++ Get_IP_Addr_from_AWS-ALL.v2.py
@@ -1,5 +1,5 @@
 # -- Get_IP_Addr_from_AWS-ALL.v2.py -- 
-#  Build CSV file from EC2 instance IP address data based on System Tag(Business Service) and Enviroment Tag(s)
+#  Build CSV file from EC2 instance IP address data based on Application/System Tag and Enviroment Tag(s)
 #  to import into Fileserver or S3 bucket for processing
 
 # python lib imports (from 2022)
@@ -18,14 +18,14 @@ ec2 = boto3.resource('ec2')
 csv_file = open("c:/my_archer_scripts_location/the_generated_daily_archer_copy.csv", "w")
 
 # variables - from AWS Tags: 
-# my_BS = System Tag in AWS => Business Service/Application/System Name
+# my_SYS = System Tag in AWS =>  Service/Application/System Name
 # my_Env = Environment Tag in AWS => Prod/Production | Stage/Staging | Dev/Development | Test/Testing
 
 # write IP list found into CSV with Business Service and Environment 
-# csv_file.write(str(my_list) + "," + my_BS + "," + my_Env + "\n")
+# csv_file.write(str(my_list) + "," + my_SYS + "," + my_Env + "\n")
 
 # Example 1 
-# BS = SharePoint_Example_01
+# my_SYS = SharePoint_Example_01
 # Env = Stage 
 # Date Added 12/21/21
 filters = [
@@ -33,7 +33,7 @@ filters = [
     {'Name': 'tag:Environment', 'Values': ['*tage']}
 ]
 my_list = []
-my_BS = "SharePoint_Example_01"
+my_SYS = "SharePoint_Example_01"
 
 # Set to Stage - - ignore case issue
 my_Env = "Stage"
@@ -41,10 +41,10 @@ for instance in ec2.instances.filter(Filters=filters):
     if instance.state["Name"] != "running":
         continue
     my_list = format(instance.private_ip_address)
-    csv_file.write(str(my_list) + "," + my_BS + "," + my_Env + "\n")
+    csv_file.write(str(my_list) + "," + my_SYS + "," + my_Env + "\n")
 
 # Example 2
-# BS = SharePoint_Example_02
+# my_SYS = SharePoint_Example_02
 # Env =  Production
 # Date Added - 08/08/1950
 filters = [
@@ -53,21 +53,21 @@ filters = [
 ]
 
 my_list = []
-my_BS = "SharePoint_Example_02"
+my_SYS = "SharePoint_Example_02"
 # Set To "Production" - ignore case issue
 my_Env = "Production"
 for instance in ec2.instances.filter(Filters=filters):
     if instance.state["Name"] != "running":
         continue
     my_list = format(instance.private_ip_address)
-    csv_file.write(str(my_list) + "," + my_BS + "," + my_Env + "\n")
+    csv_file.write(str(my_list) + "," + my_SYS + "," + my_Env + "\n")
 
 # sleep for a minute after every 10 systems added 
 #time.sleep(60) # Delay for 1 minute (60 seconds).
 
 # Example 3 -
 # All "Non Prod" systems : get Staging and Dev info with extra filtering 
-# BS = MainWebSite
+# my_SYS = MainWebSite
 # ENV =  Stage & Dev
 # Added 12/21/2021
 filters = [
@@ -81,25 +81,25 @@ filters2 = [
 ]
 
 my_list = []
-my_BS = "MainWebSite"
+my_SYS = "MainWebSite"
 # set to "Stage and Develop" for CSV 
 my_Env = "Stage and Develop"
 for instance in ec2.instances.filter(Filters=filters):
     if instance.state["Name"] != "running":
            continue
     my_list = format(instance.private_ip_address)
-    csv_file.write(str(my_list) + "," + my_BS + "," + my_Env + "\n")
+    csv_file.write(str(my_list) + "," + my_SYS + "," + my_Env + "\n")
     
 
 for instance in ec2.instances.filter(Filters=filters2):
     if instance.state["Name"] != "running":
         continue
     my_list = format(instance.private_ip_address)
-    csv_file.write(str(my_list) + "," + my_BS + "," + my_Env + "\n")
+    csv_file.write(str(my_list) + "," + my_SYS + "," + my_Env + "\n")
 
 # Example 4 - special tag/filter 
 # extra tag: "IsInSpecialNetwork" -> True
-# BS - MyExtraNetwork
+# my_SYS - MyExtraNetwork
 # Env = Stage 
 # Date Added - 08/08/1951
 filters = [
@@ -108,14 +108,14 @@ filters = [
 ]
 
 my_list = []
-my_BS = "MyExtraNetwork"
+my_SYS = "MyExtraNetwork"
 my_Env = "Stage"
 # Set to "Stage" to ignore Case 
 for instance in ec2.instances.filter(Filters=filters):
     if instance.state["Name"] != "running":
         continue
     my_list = format(instance.private_ip_address)
-    csv_file.write(str(my_list) + "," + my_BS + "," + my_Env + "\n")
+    csv_file.write(str(my_list) + "," + my_SYS + "," + my_Env + "\n")
 
 # sleep for a minute after every 10 systems 
 #time.sleep(60) # Delay for 1 minute (60 seconds).
